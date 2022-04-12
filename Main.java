1cad1bb24ec15a8ef21184c72ef281d0ccf7735c
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-04-12 14:17:22 -0600

    Part 1 complete.

--- Main.java
+++ Main.java
@@ -1,7 +1,17 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Instantiate objects here!");
+		MelonType musk = new MelonType("musk", "Muskmelon", 1998, "green", true, true);
+
+		MelonType cas = new MelonType("cas", "Casaba", 2003, "orange", true, false);
+
+		MelonType cren = new MelonType("cren", "Crenshaw", 1996, "green", false, false);
+
+		MelonType yw = new MelonType("yw", "Yellow Watermelon", 2013, "yellow", false, true);
+
+		MelonType[] types = {musk, cas, cren, yw};
+
+
 	}
 
 }
